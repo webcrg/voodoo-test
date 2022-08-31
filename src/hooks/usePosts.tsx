eae7527191e70ec8edf0e584751a23b6f27b7a58
webcrg <webcrg@yandex.ru>
2022-09-01 00:19:04 +0300

    some usepost hook improvements

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -3,6 +3,13 @@ import { getPosts, getUsers } from '@/api';
 import { IPost, IUser } from '@/interfaces';
 import { capitalizeFirst } from '@/helpers';
 
+const fetchData = async () => {
+  const postsResponse = await getPosts();
+  const usersResponse = await getUsers();
+
+  return [postsResponse.data, usersResponse.data];
+};
+
 function getUserName(post: IPost, users: IUser[]) {
   const findedUser = users.find((user) => user.id === post.userId);
   return findedUser?.name ?? '';
@@ -21,13 +28,6 @@ function usePosts() {
   const [posts, setPosts] = useState<IPost[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const postsResponse = await getPosts();
-      const usersResponse = await getUsers();
-
-      return [postsResponse.data, usersResponse.data];
-    };
-
     try {
       fetchData()
         .then(([postsResponse, usersResponse]) =>
