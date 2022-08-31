e8c5ed09ec1470ccec3215ae6facb0e59968b68b
webcrg <webcrg@yandex.ru>
2022-08-31 17:23:45 +0300

    changed jsonplaceholder base url

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://jsonplaceholder.typicode.com/';
+export const BASE_URL = 'https://jsonplaceholder.typicode.com/';
 export const ROUTES = {
   POSTS: 'posts',
   USERS: 'users',
