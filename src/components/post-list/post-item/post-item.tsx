f4cc3ad45d3a7ff407730395d996033e7e7b4c80
webcrg <webcrg@yandex.ru>
2022-09-02 22:34:20 +0300

    refactor: add memo to post-item

--- src/components/post-list/post-item/post-item.tsx
+++ src/components/post-list/post-item/post-item.tsx
@@ -9,20 +9,18 @@ interface IPostItem {
   userName: string;
 }
 
-function PostItem({ title, body, userName }: IPostItem) {
-  return (
-    <Card variant="outlined" sx={{ padding: '10px 15px' }}>
-      <Typography variant="h6" color="#5f9de3" lineHeight={1.4} mb={1}>
-        {title}
-      </Typography>
-      <Typography variant="body2" mb={1}>
-        {body}
-      </Typography>
-      <Typography variant="caption" color="text.secondary">
-        {userName}
-      </Typography>
-    </Card>
-  );
-}
+const PostItem = React.memo(({ title, body, userName }: IPostItem) => (
+  <Card variant="outlined" sx={{ padding: '10px 15px' }}>
+    <Typography variant="h6" color="#5f9de3" lineHeight={1.4} mb={1}>
+      {title}
+    </Typography>
+    <Typography variant="body2" mb={1}>
+      {body}
+    </Typography>
+    <Typography variant="caption" color="text.secondary">
+      {userName}
+    </Typography>
+  </Card>
+));
 
 export { PostItem };
